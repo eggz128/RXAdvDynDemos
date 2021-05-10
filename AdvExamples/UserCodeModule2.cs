cce7fba712a1e2023b34674a7e0948a28885b649
Steve Powell <steve.powell@edgewordstraining.co.uk>
2021-05-10 12:56:57 +0100

    Initial commit. Added comments to recording modules.

---
+++ AdvExamples/UserCodeModule2.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by Ranorex
+ * User: edgewords
+ * Date: 03/02/2021
+ * Time: 17:41
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace AdvExamples
+{
+    /// <summary>
+    /// Description of UserCodeModule2.
+    /// </summary>
+    [TestModule("27BE0005-F092-49FE-9D36-821248AD78E3", ModuleType.UserCode, 1)]
+    public class UserCodeModule2 : ITestModule
+    {
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public UserCodeModule2()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Mouse.DefaultMoveTime = 300;
+            Keyboard.DefaultKeyPressTime = 100;
+            Delay.SpeedFactor = 1.0;
+        }
+    }
+}
